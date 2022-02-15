90b6a63ad9ae8ea5e410772eca7e389856319b5c
Harris Zhang <hc_zhang@trip.com>
2022-02-15 16:42:00 +0800

    Update 0 - Build-Publish-Move.bat

--- 0 - Build-Publish-Move.bat
+++ 0 - Build-Publish-Move.bat
@@ -4,8 +4,6 @@ dotnet restore
 
 dotnet build --no-restore -c Release
 
-dotnet nuget push Panosen.Text.TextExtension\bin\Release\Panosen.Text.TextExtension.*.nupkg -s https://package.savory.cn/v3/index.json --skip-duplicate
-
-move /Y Panosen.Text.TextExtension\bin\Release\Panosen.Text.TextExtension.*.nupkg D:\LocalSavoryNuget\
+move /Y Panosen.Text.Extension\bin\Release\Panosen.Text.Extension.*.nupkg D:\LocalSavoryNuget\
 
 pause
