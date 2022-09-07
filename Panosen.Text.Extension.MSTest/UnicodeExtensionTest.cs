48f4df4d4279ceeb854748ee25b80df6cc7fd176
hc_zhang <hc_zhang@trip.com>
2022-09-07 10:54:59 +0800

    1.3.0 unicode

---
+++ Panosen.Text.Extension.MSTest/UnicodeExtensionTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panosen.Text.Extension.MSTest
+{
+    [TestClass]
+    public class UnicodeExtensionTest
+    {
+        [TestMethod]
+        public void TestToUnicode()
+        {
+            Assert.AreEqual("\\u0031", "1".ToUnicode(UnicodeFormat.Simple));
+            Assert.AreEqual("\\u0032", "2".ToUnicode(UnicodeFormat.Simple));
+            Assert.AreEqual("\\u0031\\u0032", "12".ToUnicode(UnicodeFormat.Simple));
+
+            Assert.AreEqual("&#x0031;", "1".ToUnicode(UnicodeFormat.Complex));
+            Assert.AreEqual("&#x0032;", "2".ToUnicode(UnicodeFormat.Complex));
+            Assert.AreEqual("&#x0031;&#x0032;", "12".ToUnicode(UnicodeFormat.Complex));
+        }
+
+        [TestMethod]
+        public void TestFromUnicode()
+        {
+            Assert.AreEqual("1", "\\u0031".FromUnicode());
+            Assert.AreEqual("2", "\\u0032".FromUnicode());
+            Assert.AreEqual("12", "\\u0031\\u0032".FromUnicode());
+
+            Assert.AreEqual("1", "&#x0031;".FromUnicode());
+            Assert.AreEqual("2", "&#x0032;".FromUnicode());
+            Assert.AreEqual("12", "&#x0031;&#x0032;".FromUnicode());
+
+            Assert.AreEqual("12", "&#x31;&#x32;".FromUnicode());
+        }
+    }
+}
---
+++ Panosen.Text.Extension/UnicodeExtension.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panosen.Text.Extension
+{
+    /// <summary>
+    /// Unicode格式
+    /// </summary>
+    public enum UnicodeFormat
+    {
+        /// <summary>
+        /// \u0000
+        /// </summary>
+        Simple,
+
+        /// <summary>
+        /// &amp;#x0000;
+        /// </summary>
+        Complex
+    }
+
+    /// <summary>
+    /// Unicode编解码相关
+    /// </summary>
+    public static class UnicodeExtension
+    {
+        /// <summary>
+        /// 字符串转Unicode字符串
+        /// 示例：1 => \u0031
+        /// </summary>
+        public static string ToUnicode(this string text, UnicodeFormat format = UnicodeFormat.Simple)
+        {
+            var bytes = Encoding.Unicode.GetBytes(text);
+            var builder = new StringBuilder();
+            for (var i = 0; i < bytes.Length; i += 2)
+            {
+                switch (format)
+                {
+                    case UnicodeFormat.Complex:
+                        {
+                            builder.AppendFormat("&#x{0:x2}{1:x2};", bytes[i + 1], bytes[i]);
+                        }
+                        break;
+                    case UnicodeFormat.Simple:
+                    default:
+                        {
+                            builder.AppendFormat("\\u{0:x2}{1:x2}", bytes[i + 1], bytes[i]);
+                        }
+                        break;
+                }
+
+            }
+            return builder.ToString();
+        }
+
+        /// <summary>  
+        /// Unicode字符串转为正常字符串
+        /// 示例：\u0031 => 1
+        /// </summary>
+        public static string FromUnicode(this string text)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            var items = text.Split(new string[] { "\\u", "&#x", ";" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in items)
+            {
+                var ch = (char)int.Parse(item, System.Globalization.NumberStyles.HexNumber);
+                builder.Append(ch);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
